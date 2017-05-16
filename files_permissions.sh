592e1d0e80959f435d59df58d64fb25b64c7b469
felipedemacedo <felipedemacedo.cin@gmail.com>
2017-05-16 20:33:45 -0300

    Update files_permissions.sh
    Fixed: removed 'sudo', fixed path of script execution and added verbosity

--- files_permissions.sh
+++ files_permissions.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 #give full file permission access to user redmine
-sudo chown -R redmine:redmine files log tmp public/plugin_assets
-sudo chmod -R 755 files log tmp public/plugin_assets
+cd /usr/src/redmine
+chown -v -R redmine:redmine files log tmp public/plugin_assets
+chmod -v -R 755 files log tmp public/plugin_assets
