5e8f31c435ef0cd3e04d263c65c374fa6ce943b7
Felipe de Macêdo Rodrigues <felipe.de.rodrigues@accenture.com>
2017-05-16 22:11:56 -0300

    Added script to easily download/update all 'redmine_cluster' docker images from Docker Hub.

---
+++ docker-pull-all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker pull felipederodrigues/redmine_cluster:latest
+docker pull felipederodrigues/redmine_cluster:latest_postfix
+docker pull felipederodrigues/redmine_cluster:passenger
+docker pull felipederodrigues/redmine_cluster:passenger_postfix
